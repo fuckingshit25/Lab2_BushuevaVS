cd1dc85e098e27b3f17bc0abd3da5435eb61135d
fuckingshit25 <103372417+fuckingshit25@users.noreply.github.com>
2023-10-10 19:19:03 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,126 +1,32 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Serilog;
-using Xunit.Sdk;
-using Треугольники;
-
-class TriangleCalculator
+namespace RegisterProject
 {
-    private ILogger logger;
-
-    public TriangleCalculator()
+    class Program
     {
-        // Инициализация логгера Serilog для записи в файл и консоль
-        logger = new LoggerConfiguration()
-            .WriteTo.Console()
-            .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
-            .CreateLogger();
-    }
-
-    public (string, (int, int)[]) CalculateTriangle(string sideA, string sideB, string sideC)
-    {
-        logger.Information("Запрос: сторона A = {SideA}, сторона B = {SideB}, сторона C = {SideC}", sideA, sideB, sideC);
-        string triangleType = "";
-        (int, int)[] vertices = new (int, int)[3];
-        double a, b, c;
-
-        if (double.TryParse(sideA, out a) && double.TryParse(sideB, out b) && double.TryParse(sideC, out c))
+        static void Main(string[] args)
         {
-            try
-            {
-                // Проверка на неотрицательность сторон
-                if (a <= 0 || b <= 0 || c <= 0)
-                {
-                    logger.Error("Ошибка: одна или несколько сторон имеют недопустимое значение");
-                }
-
-                // Проверка на существование треугольника
-                if (a + b > c && b + c > a && c + a > b)
-                {
-
-                    if (a == b && b == c)
-                    {
-                        triangleType = "равносторонний";
-                    }
-                    else if (a == b || b == c || c == a)
-                    {
-                        triangleType = "равнобедренный";
-                    }
-                    else
-                    {
-                        triangleType = "разносторонний";
-                    }
-
-                    // Вычисление углов треугольника
-                    double alpha = Math.Acos((b * b + c * c - a * a) / (2 * b * c));
-                    double beta = Math.Acos((c * c + a * a - b * b) / (2 * c * a));
-                    double gamma = Math.PI - alpha - beta;
-                    // Вычисление координат вершин треугольника
-                    int scalingFactor = 100;
-                    vertices[0] = (0, 0);
-                    vertices[1] = (scalingFactor, 0);
-                    vertices[2] = ((int)(scalingFactor * Math.Cos(alpha)), (int)(scalingFactor * Math.Sin(alpha)));
-                    logger.Information(@"Результат: Тип треугольника - {triangleType}, Координаты вершин: A({0},{1}), B({2},{3}), C({4},{5})", triangleType, vertices[0].Item1, vertices[0].Item2, vertices[1].Item1, vertices[1].Item2, vertices[2].Item1, vertices[2].Item2);
-                }
-                else
-                {
-                    logger.Error("Ошибка: треугольник с заданными сторонами не существует");
-                    return ("Ошибка", new (int, int)[3] { (-2, -2), (-2, -2), (-2, -2) });
-                }
+            UnitTests tests = new UnitTests();
             
-            }
-            catch (Exception ex)
-            {
-                logger.Error("Ошибка при вычислении треугольника: {ErrorMessage}", ex.Message);
-                return ("Ошибка", new (int, int)[3] { (-2, -2), (-2, -2), (-2, -2) });
-            }
+            tests.CheckRegister_EmptyLogin_ReturnsFalseAndErrorMessage();
+            tests.CheckRegister_ShortLogin_ReturnsFalseAndErrorMessage();
+            tests.CheckRegister_InvalidPhoneLogin_ReturnsFalseAndErrorMessage();
+            tests.CheckRegister_ExistingLogin_ReturnsFalseAndErrorMessage();
+            tests.CheckRegister_ValidData_ReturnsTrue();
+            tests.CheckRegister_NotValidPass_1();
+            tests.CheckRegister_NotValidPass_2();
+            tests.CheckRegister_NotValidPass_3();
+            tests.CheckRegister_NotValidPass_4();
+            tests.CheckRegister_NotValidPass_5();
+            tests.CheckRegister_NotValidPasswordEquality();
+            tests.CheckRegister_NotValidPasswordLength();
+            tests.CheckRegister_InvalidEmailLogin_ReturnsFalseAndErrorMessage();
+            tests.CheckRegister_InvalidSymbolInLogin();
+            tests.CheckRegister_EmptyPassword_ReturnsFalseAndErrorMessage();
+            tests.CheckRegister_ValidLoginAndPassword();
+            tests.CheckRegister_ValidMailAndPassword();
+            tests.CheckRegister_ValidPhoneAndPassword();
+            tests.CheckRegister_EmptyLoginAndPassword();
+            tests.CheckRegister_EmptyLogin();
         }
-        else
-        {
-            logger.Error("Ошибка: невозможно распознать входные данные сторон треугольника");
-            return ("Ошибка", new (int, int)[3] { (-2, -2), (-2, -2), (-2, -2) });
-        }
-
-        return (triangleType, vertices);
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        ILogger logger;
-        // Инициализация логгера Serilog для записи в файл и консоль
-        logger = new LoggerConfiguration()
-            .WriteTo.Console()
-            .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
-            .CreateLogger();
-        string a="";
-        string b="";
-        string c="";
-        var testClass = new TriangleCalculatorTests();
-        testClass.Initialize();
-        // Вызываем методы тестов
-        testClass.CalculateTriangle_EquilateralTriangle_ReturnsCorrectTriangleTypeAndVertices(null);
-        testClass.CalculateTriangle_IsoscelesTriangle_ReturnsCorrectTriangleTypeAndVertices();
-        testClass.CalculateTriangle_ScaleneTriangle_ReturnsCorrectTriangleTypeAndVertices();
-        testClass.CalculateTriangle_InvalidSideValues_ReturnsErrorMessageAndEmptyVertices();
-        testClass.CalculateTriangle_NegativeSideValues_ReturnsErrorMessageAndEmptyVertices();
-        testClass.CalculateTriangle_InvalidSideFormat_ReturnsErrorMessageAndEmptyVertices();
-        testClass.CalculateTriangle_InvalidTriangle_ReturnsNotTriangleMessageAndEmptyVertices();
-        testClass.CalculateTriangle_ExceptionThrown_ReturnsErrorMessageAndEmptyVertices();
-        testClass.CalculateTriangle_MinimumSideValues_ReturnsNotTriangleMessageAndEmptyVertices();
-        testClass.CalculateTriangle_MaximumSideValues_ReturnsErrorMessageAndEmptyVertices();
-        testClass.CalculateTriangle_NegativeSideValues_ReturnsNotTriangleMessageAndEmptyVertices();
-        testClass.CalculateTriangle_ZeroSideValues_ReturnsNotTriangleMessageAndEmptyVertices();
-        testClass.CalculateTriangle_TriangleWithNegativeArea_ReturnsNegativeAreaMessageAndCorrectVertices();
-        testClass.CalculateTriangle_TriangleWithZeroArea_ReturnsZeroAreaMessageAndCorrectVertices();
-        testClass.CalculateTriangle_TriangleWithMaximumArea_ReturnsMaximumAreaMessageAndCorrectVertices();
-        testClass.CalculateTriangle_TriangleWithMinimumArea_ReturnsMinimumAreaMessageAndCorrectVertices();
-        testClass.CalculateTriangle_TriangleWithEmptyValues_ReturnsAnErrorMessage();
-        testClass.CalculateTriangle_NotTriangle_ReturnsNotTriangleMessageAndEmptyVertices();
-        testClass.CalculateTriangle_RightTriangle_ReturnsCorrectTriangleTypeAndVertices();
-        testClass.CalculateTriangle_RightTriangle_ReturnsCorrectTriangleTypeAndVertices2();
-        testClass.CalculateTriangle_IsoscelesTriangle_ReturnsCorrectTriangleTypeAndVertices2();
     }
 }
---
+++ UnitTests.cs
@@ -0,0 +1,195 @@
+﻿using NUnit.Framework;
+namespace RegisterProject
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        Register Register = new Register();
+        [Test]
+        public void CheckRegister_EmptyLogin_ReturnsFalseAndErrorMessage()
+        {
+            string login = "";
+            string password = "Test123!";
+            string password2 = "Test123!";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual(("False", "Пустая строка в качестве логина"), result);
+        }//1
+
+        [Test]
+        public void CheckRegister_ShortLogin_ReturnsFalseAndErrorMessage()
+        {
+            string login = "user";
+            string password = "Test123!";
+            string password2 = "Test123!";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual(("False", "Длина логина меньше 5 символов"), result);
+        }//2
+
+        [Test]
+        public void CheckRegister_ExistingLogin_ReturnsFalseAndErrorMessage()
+        {
+            string login = "user11";
+            string password = "Test123!";
+            string password2 = "Test123!";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual(("False", "Пользователь с таким логином уже зарегистрирован"), result);
+        }//3
+
+        [Test]
+        public void CheckRegister_InvalidPhoneLogin_ReturnsFalseAndErrorMessage()
+        {
+            string login = "+1234567890";
+            string password = "Test123!";
+            string password2 = "Test123!";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual(("False", "Номер телефона не удовлетворяет заданному формату +x-xxx-xxx-xxxx"), result);
+        }//4
+        public void CheckRegister_ValidData_ReturnsTrue()
+        {
+            string login = "user123";
+            string password = "Password123!";
+            string password2 = "Password123!";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual("True", result.Item1);
+            Assert.AreEqual("", result.Item2);
+        }//5
+        public void CheckRegister_NotValidPass_1()
+        {
+            string login = "user123";
+            string password = "Password123";
+            string password2 = "Password123";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual("False", result.Item1);
+            Assert.AreEqual("В пароле отсутствует минимум один специальный символ", result.Item2);
+        }//6
+        public void CheckRegister_NotValidPass_2()
+        {
+            string login = "user123";
+            string password = "Password";
+            string password2 = "Password";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual("False", result.Item1);
+            Assert.AreEqual("В пароле отсутствует минимум одна цифра", result.Item2);
+        }//7
+        public void CheckRegister_NotValidPass_3()
+        {
+            string login = "user123";
+            string password = "password";
+            string password2 = "password";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual("False", result.Item1);
+            Assert.AreEqual("В пароле отсутствует минимум одна заглавная буква", result.Item2);
+        }//8
+        public void CheckRegister_NotValidPass_4()
+        {
+            string login = "user123";
+            string password = "PPPPPPP";
+            string password2 = "PPPPPPP";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual("False", result.Item1);
+            Assert.AreEqual("В пароле отсутствует минимум одна строчная буква", result.Item2);
+        }//9
+        public void CheckRegister_NotValidPass_5()
+        {
+            string login = "user123";
+            string password = "Password猫";
+            string password2 = "Password猫";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual("False", result.Item1);
+            Assert.AreEqual("В пароле присутствуют недопустимые символы", result.Item2);
+        }//10
+        public void CheckRegister_NotValidPasswordEquality()
+        {
+            string login = "user123";
+            string password = "Password123!";
+            string password2 = "Password1234!";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual("True", result.Item1);
+            Assert.AreEqual("Заданные пароли не совпадают", result.Item2);
+        }//11
+        public void CheckRegister_NotValidPasswordLength()
+        {
+            string login = "user123";
+            string password = "Pass1!";
+            string password2 = "Pass1!";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual("False", result.Item1);
+            Assert.AreEqual("Длина пароля меньше 7 символов", result.Item2);
+        }//12
+        [Test]
+        public void CheckRegister_InvalidEmailLogin_ReturnsFalseAndErrorMessage()
+        {
+            string login = "user@example";
+            string password = "Test123!";
+            string password2 = "Test123!";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual("False", result.Item1);
+            Assert.AreEqual("Email не удовлетворяет общему формату xxx@xxx.xxx", result.Item2);
+        }//13
+
+        [Test]
+        public void CheckRegister_InvalidSymbolInLogin()
+        {
+            string login = "user猫test123";
+            string password = "Test123!";
+            string password2 = "Test123!";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual(("False", "Логин содержит символы, отличные от латиницы, цифр и знака подчеркивания"), result);
+        }//14
+
+        [Test]
+        public void CheckRegister_EmptyPassword_ReturnsFalseAndErrorMessage()
+        {
+            string login = "testuser";
+            string password = "";
+            string password2 = "";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual(("False", "Пустая строка в качестве пароля"), result);
+        }//15
+        [Test]
+        public void CheckRegister_ValidLoginAndPassword()
+        {
+            string login = "testuser";
+            string password = "Test123!";
+            string password2 = "Test123!";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual(("True", ""), result);
+        }//16
+        [Test]
+        public void CheckRegister_ValidMailAndPassword()
+        {
+            string login = "testuser@mail.ru";
+            string password = "Test123!";
+            string password2 = "Test123!";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual(("True", ""), result);
+        }//17
+        [Test]
+        public void CheckRegister_ValidPhoneAndPassword()
+        {
+            string login = "+7-963-843-5647";
+            string password = "Test123!";
+            string password2 = "Test123!";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual(("True", ""), result);
+        }//18
+        [Test]
+        public void CheckRegister_EmptyLoginAndPassword()
+        {
+            string login = "";
+            string password = "";
+            string password2 = "";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual(("False", "Пустая строка в качестве логина"), result);
+        }//19
+        [Test]
+        public void CheckRegister_EmptyLogin()
+        {
+            string login = "猫猫猫猫猫猫猫";
+            string password = "";
+            string password2 = "";
+            var result = Register.CheckRegister(login, password, password2);
+            Assert.AreEqual(("False", "Логин содержит символы, отличные от латиницы, цифр и знака подчеркивания"), result);
+        }//20
+    }
+}
